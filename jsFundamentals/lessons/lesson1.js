dfb273dc5b5be35337f32054492fdb6aad5c4046
NOV\maliab <Aditya.mali@nov.com>
2023-05-08 22:55:40 +0530

    updated comments

--- jsFundamentals/lessons/lesson1.js
+++ jsFundamentals/lessons/lesson1.js
@@ -2,8 +2,8 @@
 console.log("Hello World!")
 
 //Variables
-var firstName = "Aditya"
-let lastName = "Mali"
+var firstName = "Aditya1"
+let lastName = "Mali2"
 console.log(firstName, lastName)
 
 var age, sex
@@ -13,6 +13,13 @@ console.log(age, sex)
 age = "26"
 console.log(age, sex)
 
-//Constants
+//Constants - Value of a constant can't be changed during runtime
 const occupation = "engineer";
 console.log(occupation)
+
+//DataTypes
+var middleName = 'Bhalchandra'  //string
+var number = 12                 //number
+var isMarried = false           //boolean
+var noValue = null              //null - no value 
+var numberOfCarsInWorld = undefined   //undefined
