4c449dc0070e949ff6b6509ed37e9a680d3b77b4
NOV\maliab <Aditya.mali@nov.com>
2023-06-22 19:06:02 +0530

    lesson 3

---
+++ jsFundamentals/lessons/lesson2.js
@@ -0,0 +1,6 @@
+var price = 50
+var product = "Bicycle"
+var messageToPrint1 = "The value of this "+product+" is "+price+" dollars"      //Concatination
+var messageToPrint2 = `The value of this ${product} is ${price} dollars`        //Interpolation
+console.log(messageToPrint1);
+console.log(messageToPrint2);
